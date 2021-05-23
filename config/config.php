76e6be688c0db83a0096b2da180559cfbcf11539
sergeykotenko <sergeykotenko@bk.ru>
2021-05-23 03:30:13 +0300

    animate

--- config/config.php
+++ config/config.php
@@ -1,9 +1,25 @@
 <?php
+
+// Пути файлов
+
 define('TEMPLATES_DIR', '../templates/');
 define('LAYOUT_DIR', 'layouts/main');
 
-include "../engine/function.php";
+// Вывод сообщений об отправки
+
+$messageUpload = [
+    'OK' => 'Файл успешно загружен',
+    'ERROR' => 'Ошибка',
+    'JPG' => 'Можно загружать только jpg-файлы',
+    'DOUBLE' => 'Файл существует, выберите другое имя файла!',
+    'BIGSIZE' => 'Размер файла не больше 5 мб',
+    'NOTPHP' => 'Загрузка php-файлов запрещена!',
+    'NOTIMG' => 'Можно загружать только jpg-файлы, неверное содержание файла, не изображение',
+];
+
+// Подключение модулей
 include "../engine/menu.php";
+include "../engine/function.php";
 include "../engine/getFileImg.php";
 include "../engine/classSimpleImage.php";
 include "../engine/uploadFile.php";
