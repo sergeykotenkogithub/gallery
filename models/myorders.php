e86609b5c11906472b4b490a79515c9bf3d2b88d
sergeykotenko <sergeykotenko@bk.ru>
2021-06-05 00:02:19 +0300

    'myorder'

--- models/myorders.php
+++ models/myorders.php
@@ -1,6 +1,6 @@
 <?php
 
 function getMyorders($id) {
-    $sql = "SELECT o.users_id, o.hash, g.name, o.id FROM orders o JOIN basket b on b.session_id = o.hash join goods g on g.id = b.goods_id WHERE o.users_id = {$id} ORDER BY o.id";
+    $sql = "SELECT o.hash, g.name, o.id, b.quantity, g.image, b.price FROM orders o JOIN basket b on b.session_id = o.hash join goods g on g.id = b.goods_id WHERE o.users_id = 2 ORDER BY o.id";
     return getAssocResult($sql);
 }
--- templates/myorders.php
+++ templates/myorders.php
@@ -7,7 +7,18 @@
             <?php else: ?>
              <div>Заказ №: <?="{$order[$i]['id']}"?></div>
             <?endif;?>
-            <div><?="{$order[$i]['name']}"?></div>
+
+        <div class="basket">
+            <div class="basket__name"><?="{$order[$i]['name']}"?> </div>
+            <div>
+                <img src="/img/goods/<?="{$order[$i]['image']}"?>" width="100">
+            </div>
+            <div class="rub basket__price"><?="{$order[$i]['price']}"?></div>
+            <div class="basket__quantity">Кол-во:<?="{$order[$i]['quantity']}"?></div>
+<!--            <div class="basket__quantity">:--><?//="{$order[$i]['quantity']}"?><!--</div>-->
+        </div>
+
+
         <?endfor;?>
     </div>
 </div>
