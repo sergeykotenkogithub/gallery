e6fade025b4cf34590f166c6fba7960ad08758f3
sergeykotenko <sergeykotenko@bk.ru>
2021-06-06 12:39:31 +0300

    'beforeauth'

--- models/goods.php
+++ models/goods.php
@@ -1,5 +1,7 @@
 <?php
 
+// Вывод всех товаров
+
 function getAllCatalog() {
     $sql = "SELECT * FROM `goods`";
     return getAssocResult($sql);
@@ -14,10 +16,6 @@ function getOneCatalog($id) {
 function getItemFeedback($id) {
     return getOneResult("SELECT g.id, g.name, f.name, f.feedback FROM goods g join feedback f on g.id = f.goods_id WHERE g.id = {$id}");
 }
-
-// Добавление товара в корзину
-
-
 
 function comparisonGoodsBasket($id, $session) {
     $sql = "SELECT goods_id FROM basket where goods_id = '{$id}' AND session_id = '{$session}'";
