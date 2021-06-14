ecc0f123dad0604605b4ed06b2febfcd836a675d
sergeykotenko <sergeykotenko@bk.ru>
2021-06-14 03:43:23 +0300

    'before_tabs'

--- templates/basket.php
+++ templates/basket.php
@@ -1,7 +1,7 @@
 <div class="container">
-    Товаров в корзине: <?=$count?>
-    <br>
-    <br>
+
+    <div class="basket__title">Товаров в корзине: <?=$count?></div>
+
     <?php foreach ($basket as $item): ?>
     <div class="basket">
         <div class="basket__name"><?=$item['name']?> </div>
@@ -23,13 +23,10 @@
     <?php endforeach;?>
 
     <? if (!$show):?>
-    <div class="rub total">Итого: <?=$summ['summ']?> </div>
+        <div class="rub total">Итого: <?=$summ['summ']?> </div>
+        <div class="order"><a href="/order">Оформить заказ</a></div>
     <?else:?>
-    <div>Вы ещё не добавили товар</div>
-    <?endif;?>
-
-    <? if (!$show): ?>
-    <div class="order"><a href="/order">Оформить заказ</a></div>
+        <div>Вы ещё не добавили товар</div>
     <?endif;?>
 
 </div>
